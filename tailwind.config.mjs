5e7c5e35822bfbb3b0ac25dd028b1bdc404f7a87
qoorus <q7254task@gmail.com>
2023-12-31 17:02:40 +0900

    ヘッダーを導入

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,8 +4,38 @@ export default {
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 		'./node_modules/preline/preline.js',
 	],
+	darkMode: 'class',
 	theme: {
-		extend: {},
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		colors: {
+			'blue': '#1fb6ff',
+			'purple': '#7e5bef',
+			'pink': '#ff49db',
+			'orange': '#ff7849',
+			'green': '#13ce66',
+			'yellow': '#ffc82c',
+			'gray-dark': '#273444',
+			'gray': '#8492a6',
+			'gray-light': '#d3dce6',
+		},
+		fontFamily: {
+			sans: ['Graphik', 'sans-serif'],
+			serif: ['Merriweather', 'serif'],
+		},
+		extend: {
+			spacing: {
+				'128': '32rem',
+				'144': '36rem',
+			},
+			borderRadius: {
+				'4xl': '2rem',
+			}
+		}
 	},
 	plugins: [
 		require('preline/plugin'),
